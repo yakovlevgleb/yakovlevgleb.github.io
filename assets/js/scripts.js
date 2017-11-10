beec17a24f527b1f14f0985c4464ae6b73b9d720
Yakovlev Gleb <brin@ya.ru>
2017-11-10 18:04:46 +0500

    new design

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+/*!
+ * portf
+ * My portfolio
+ * https://github.com/yakovlevgleb/portf
+ * @author Gleb Yakovlev
+ * @version 1.0.2
+ * Copyright 2017. MIT licensed.
+ */
+// let tl = new TimelineLite();
+// tl
+//   .fromTo('.intro', 1.3, {scale: 0.01},{scale: 1})
+//   .staggerFrom('.preview', 0.3, {scale:0, autoAlpha:0}, 0.1, "stagger");
