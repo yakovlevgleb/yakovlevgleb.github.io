fc980bc607c8e0c11dc073ed67149e2852367c1a
Yakovlev Gleb <brin@ya.ru>
2017-12-05 17:27:02 +0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,7 @@
  * @version 1.0.2
  * Copyright 2017. MIT licensed.
  */
-// let tl = new TimelineLite();
-// tl
-//   .fromTo('.intro', 1.3, {scale: 0.01},{scale: 1})
-//   .staggerFrom('.preview', 0.3, {scale:0, autoAlpha:0}, 0.1, "stagger");
+let tl = new TimelineLite();
+tl
+  .fromTo('.intro', 1.3, {scale: 0.01},{scale: 1})
+  .staggerFrom('.preview', 0.3, {scale:0, autoAlpha:0}, 0.1, "stagger");
